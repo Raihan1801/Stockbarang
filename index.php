3fca81703fa3e62f47fecbe1be0c26cad0e7e8c5
Raihan1801 <abualiraihan1801@gmail.com>
2022-04-11 10:44:48 +0700

    second

---
+++ index.php
@@ -0,0 +1,262 @@
+<?php
+require 'function.php';
+require 'cek.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Dashboard - SB Admin</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.html">Inventori Creative Media</a>
+            <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#">&nbsp; &nbsp;<i class="fas fa-bars"></i></button>
+
+        </nav>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fa fa-strikethrough"></i></div>
+                                  Stock Barang
+                            <a class="nav-link" href="masuk.php">
+                                <div class="sb-nav-link-icon"><i class="fa fa-arrow-circle-right"></i></div>
+                                    Barang Masuk
+                            <a class="nav-link" href="keluar.php">
+                                <div class="sb-nav-link-icon"><i class="fa fa-arrow-circle-up"></i></div>
+                                    Barang Keluar
+                            <a class="nav-link" href="admin.php">
+                                <div class="sb-nav-link-icon"><i class="fa fa-user-circle"></i></div>
+                                    Kelola Admin
+                            <a class="nav-link" href="logout.php">
+                                    Logout
+                            </a>
+
+                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
+                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
+                                        Authentication
+                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                    </a>
+                                    <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
+                                        <nav class="sb-sidenav-menu-nested nav">
+                                            <a class="nav-link" href="login.html">Login</a>
+                                            <a class="nav-link" href="register.html">Register</a>
+                                            <a class="nav-link" href="password.html">Forgot Password</a>
+                                        </nav>
+                                    </div>
+                                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
+                                        Error
+                                        <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                                    </a>
+                                    <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">
+                                        <nav class="sb-sidenav-menu-nested nav">
+                                            <a class="nav-link" href="401.html">401 Page</a>
+                                            <a class="nav-link" href="404.html">404 Page</a>
+                                            <a class="nav-link" href="500.html">500 Page</a>
+                                        </nav>
+                                    </div>
+                                </nav>
+
+                    </div>
+                </nav>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h1 class="mt-4">Stock Barang</h1>
+
+                        <div class="card mb-4">
+                            <div class="card-header">
+                              <!-- Button to Open the Modal -->
+                                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+                                    Tambah Barang
+                                  </button>
+                                    <a href="export.php" class="btn btn-outline-info">Cetak laporan</a>
+                                  </button>
+                            </div>
+                            <div class="card-body">
+
+                              <?php
+                                $ambildatastock = mysqli_query($conn,"select * from stock where stock < 1");
+
+                                while($fetch=mysqli_fetch_array($ambildatastock)){
+                                  $barang = $fetch['namabarang'];
+
+                               ?>
+                              <div class="alert alert-danger alert-dismissible">
+                                  <button type="button" class="close" data-dismiss="alert">&times;</button>
+                                  <strong>Perhatian!</strong> Stock <?=$barang;?> Telah Habis.
+                                  </div>
+                              <?php
+                                  }
+                               ?>
+
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Nama Barang</th>
+                                                <th>Deksripsi</th>
+                                                <th>Stock</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                          <?php
+                                          $ambilsemuadatastock = mysqli_query($conn,"select * from stock");
+                                            $i = 1;
+                                          while($data=mysqli_fetch_array($ambilsemuadatastock)){
+                                            $namabarang = $data['namabarang'];
+                                            $deksripsi = $data['deksripsi'];
+                                            $stock = $data['stock'];
+                                            $idb = $data['idbarang'];
+
+                                           ?>
+
+
+                                            <tr>
+                                                <td><?=$i++?></td>
+                                                <td><?=$namabarang;?></td>
+                                                <td><?=$deksripsi;?></td>
+                                                <td><?=$stock;?></td>
+                                                <td>
+                                                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#edit<?=$idb;?>">
+                                                        Edit
+                                                      </button>
+                                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#delete<?=$idb;?>">
+                                                        Delete
+                                                      </button>
+
+                                                </td>
+                                            </tr>
+
+                                            <!-- Edit The Modal -->
+                                            <div class="modal fade" id="edit<?=$idb;?>">
+                                              <div class="modal-dialog">
+                                                <div class="modal-content">
+
+                                                  <!-- Modal Header -->
+                                                  <div class="modal-header">
+                                                    <h4 class="modal-title">Edit Barang</h4>
+                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                  </div>
+
+                                                  <!-- Modal body -->
+                                                  <form method="post">
+                                                  <div class="modal-body">
+                                                    <input type="text" name="namabarang" value="<?=$namabarang;?>" class="form-control" required>
+                                                    <br>
+                                                    <input type="text" name="deksripsi" value="<?=$deksripsi;?>" class="form-control" required>
+                                                    <br>
+                                                    <input type="number" name="stock" value="<?=$stock;?>" class="form-control" required>
+                                                    <br>
+                                                    <input type="hidden" name="idb" value="<?=$idb;?>">
+                                                    <button type="submit" class="btn btn-primary" name="updatebarang">Update</button>
+                                                  </div>
+                                                </form>
+                                              </div>
+                                              </div>
+                                            </div>
+
+                                            <!-- Delete Modal -->
+                                            <div class="modal fade" id="delete<?=$idb;?>">
+                                              <div class="modal-dialog">
+                                                <div class="modal-content">
+
+                                                  <!-- Modal Header -->
+                                                  <div class="modal-header">
+                                                    <h4 class="modal-title">Hapus Barang?</h4>
+                                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                                  </div>
+
+                                                  <!-- Modal body -->
+                                                  <form method="post">
+                                                  <div class="modal-body">
+                                                    Apakah anda yakin ingin menghapus <?=$namabarang;?>?
+                                                    <input type="hidden" name="idb" value="<?=$idb;?>">
+                                                    <br>
+                                                    <br>
+                                                    <button type="submit" class="btn btn-danger" name="hapusbarang">Hapus</button>
+                                                  </div>
+                                                </form>
+                                              </div>
+                                              </div>
+
+
+
+                                          </div>
+
+
+                                          <?php
+                                        };
+                                           ?>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </main>
+                <footer class="py-4 bg-light mt-auto">
+                    <div class="container-fluid">
+                        <div class="d-flex align-items-center justify-content-between small">
+                            <div class="text-muted">Copyright &copy; Creative Media 2022</div>
+                            <div>
+
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/datatables-demo.js"></script>
+    </body>
+
+    <!-- The Modal -->
+    <div class="modal fade" id="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h4 class="modal-title">Tambah Barang</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+
+          <!-- Modal body -->
+          <form method="post" enctype="multipart/form-data">
+          <div class="modal-body">
+            <input type="text" name="namabarang" placeholder="Nama Barang" class="form-control" required>
+            <br>
+            <input type="text" name="deksripsi" placeholder="Deksripsi Barang" class="form-control" required>
+            <br>
+            <input type="number" name="stock" placeholder="Stock" class="form-control" required>
+            <br>
+            <button type="submit" class="btn btn-primary" name="addnewbarang">Submit</button>
+          </div>
+        </form>
+
+
+</html>
